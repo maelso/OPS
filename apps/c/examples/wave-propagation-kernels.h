dcc9c94058bb22042488ba2ab4226210fa1b3b7e
maelso <maelso.bruno@gmail.com>
2019-03-14 11:43:11 -0300

    Creating initialize_velocity_model_kernel and initialize_damp_kernel

--- apps/c/examples/wave-propagation-kernels.h
+++ apps/c/examples/wave-propagation-kernels.h
@@ -1,6 +1,48 @@
 #ifndef WAVE_PROPAGATION_KERNELS_H
 #define WAVE_PROPAGATION_KERNELS_H
 
+void initialize_velocity_model_kernel(float *m){
+    m[OPS_ACC0(0, 0, 0)] = 0.25;
+}
+
+void initialize_damp_kernel(float *damp, int *idx){
+    float dampcoeff;
+    float pos;
+    float val;
+    int pivot;
+
+    int damp_left_initial = space_order/2;
+    int damp_left_final = space_order/2 + border_size;
+    int damp_right_initial = damp_left_final + X_size;
+    int damp_right_final = damp_right_initial + border_size;
+
+    dampcoeff = 0.2590408229618301; // 1.5 * log(1.0 / 0.001) / (40.)
+    damp[OPS_ACC0(0, 0, 0)] = 0.0;
+
+    if ( (idx[0] >= damp_left_initial && idx[1] >= damp_left_initial && idx[2] >= damp_left_initial) && 
+         (idx[0] < damp_right_final && idx[1] < damp_right_final && idx[2] < damp_right_final)
+    ){
+        if( (idx[2] >= damp_left_initial && idx[2] < damp_left_final) || (idx[2] >= damp_right_initial && idx[2] < damp_right_final) ){
+            pivot = idx[2];
+            pos = abs((border_size - pivot + 2) / (float)border_size);
+            val = dampcoeff * (pos - sin(2 * M_PI * pos) / (2 * M_PI));
+            damp[OPS_ACC0(0, 0, 0)] = pos;
+        }
+        else if( (idx[1] >= damp_left_initial && idx[1] < damp_left_final) || (idx[1] >= damp_right_initial && idx[1] < damp_right_final) ){
+            pivot = idx[1];
+            pos = abs((border_size - pivot + 2) / (float)border_size);
+            val = dampcoeff * (pos - sin(2 * M_PI * pos) / (2 * M_PI));
+            damp[OPS_ACC0(0, 0, 0)] = pos;
+        }
+        else if( (idx[0] >= damp_left_initial && idx[0] < damp_left_final) || (idx[0] >= damp_right_initial && idx[0] < damp_right_final) ){
+            pivot = idx[0];
+            pos = abs((border_size - pivot + 2) / (float)border_size);
+            val = dampcoeff * (pos - sin(2 * M_PI * pos) / (2 * M_PI));
+            damp[OPS_ACC0(0, 0, 0)] = pos;
+        }
+    }
+}
+
 void wave_propagation_kernel(const float *dampx0, const float *mx0, float *ut10, const float *ut00, const float *ut20, int *idx)
 {
     // ut10[OPS_ACC2(0, 0, 0)] = 1.0;
